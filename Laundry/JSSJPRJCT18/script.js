8e59856a0a55f63ab15ae617cf257bcfd3400997
Neel-gohqin <neelnaymar@gmail.com>
2025-04-28 22:03:37 +0530

    adding for - PJ18UPSKMF

---
+++ Laundry/JSSJPRJCT18/script.js
@@ -0,0 +1,14 @@
+console.log(window.scrollX)
+console.log(window.scrollY)
+
+
+
+
+// let arr = [ 1, 2,3,4,5,6,7,8,9,10,11];
+//  arr.filter((n)=>{
+//     if(n>8){
+//         return true
+//     }
+//     return false
+// }),
+// console.log(arr);
