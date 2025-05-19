1fbb8005121d00a9efd144bf2651b0a304b67e48
Neel-gohqin <neelnaymar@gmail.com>
2025-05-20 00:09:30 +0530

    adding changes 3.O

--- Laundry/JSSJPRJCT18/script.js
+++ Laundry/JSSJPRJCT18/script.js
@@ -1,8 +1,27 @@
-console.log(window.scrollX)
-console.log(window.scrollY)
+//BOOK NOW BUTTON
+let Sbtn = document.querySelector(".SeconeInner button");
+Sbtn.addEventListener("click", () => {
+  window.scrollTo({ top: 700, behavior: "smooth" });
+  // window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+});
+
+const input1Fld = document.getElementById("USerId");
+let DisplayItemAddcart = document.getElementById("DisplayItemsAdd");
 
 
 
+input1Fld.addEventListener('click', () => {
+
+    DisplayItemAddcart.removeChild(DisplayItemAddcart.querySelector("img")) //remove image
+    DisplayItemAddcart.querySelectorAll("span").forEach(span =>{        
+    DisplayItemAddcart.removeChild(span)
+        //getting all span and remove
+     })
+    let listItem = DisplayItemAddcart.document.createElemt('ul');
+});
+
+
+ 
 
 // let arr = [ 1, 2,3,4,5,6,7,8,9,10,11];
 //  arr.filter((n)=>{
