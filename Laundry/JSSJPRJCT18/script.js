ff51a12e1a1c855cf53c429dc8d7ee8f40eb555c
Neel-gohqin <neelnaymar@gmail.com>
2025-08-17 18:51:13 +0530

    adding changes 3.O.8

--- Laundry/JSSJPRJCT18/script.js
+++ Laundry/JSSJPRJCT18/script.js
@@ -8,7 +8,7 @@ Sbtn.addEventListener("click", () => {
 const input1Fld = document.getElementById("USerId");
 let DisplayItemAddcart = document.getElementById("DisplayItemAddcart");
 let UserAcCollection = document.querySelector(".UserAcCollection");
-let CartList = DisplayItemAddcart.querySelector("ul");
+let CartList = DisplayItemAddcart.querySelector("ol"); //Selecting Ol List By default
 
 let popup = document.createElement("span");
 popup.classList.add("popUp");
@@ -59,7 +59,6 @@ let ServicesList = [
 
 // Select all service images
 let Items = document.querySelectorAll(".services img");
-CartList = DisplayItemAddcart.querySelector("ol");
 let PriceShowFtbar = document.querySelector(".PriceShowFtbar .Pricetag");
 PriceShowFtbar.innerText = "$0.00";
 let pc = 0;
@@ -95,8 +94,8 @@ Items.forEach((item, index) => {
     let ListService = document.createElement("li");
     ListService.classList.add("ListLi_Service");
     ListService.setAttribute("data-index", index); // Creating data index for tracking index
-    ListService.innerHTML = `<span>${selectedServices.ServiceName} </span>  <span>${selectedServices.ServicePrice}</span>`;
-    CartList.appendChild(ListService);
+    ListService.innerHTML = `<div><span>${selectedServices.ServiceName} </span>  <span>${selectedServices.ServicePrice}</span></div>`;
+    CartList.appendChild(ListService); //appending the list in Ol
 
     UserAcCollection.style.paddingBottom = "12px";
   });
@@ -104,42 +103,66 @@ Items.forEach((item, index) => {
 
 // let itemStatus = document.querySelector('.services span')
 let ItemCurrent_Status = document.querySelectorAll(".btnPlz img");
-let defaultImg = "assets/PLZuzzu.png";
+let adplzmz = document.querySelectorAll(".services span");
 
 ItemCurrent_Status.forEach((crrntSts, index) => {
   crrntSts.addEventListener("click", () => {
     if (crrntSts.src.includes("PLZuzzu.png")) {
       crrntSts.src = "assets/miNUZ.png";
+      adplzmz[index].style.color = "red";
     } else {
       crrntSts.src = "assets/PLZuzzu.png";
+      adplzmz[index].style.color = "#0d5b84";
       const itemIndexOn_Remove = index;
       const listItemsRemove = document.querySelectorAll("li");
 
+
       listItemsRemove.forEach((listItemsR) => {
         if (parseInt(listItemsR.getAttribute("data-index")) === itemIndexOn_Remove) {
-          
-          
           listItemsR.remove();
-           selectedServices = ServicesList[index];
-           TTprice = parseFloat(selectedServices.ServicePrice.replace("$", ""));
+          selectedServices = ServicesList[index];
+          TTprice = parseFloat(selectedServices.ServicePrice.replace("$", ""));
           pc -= TTprice;
           PriceShowFtbar.innerText = `$${pc.toFixed(2)}`;
+        
         }
       });
 
-      if(CartList.children.length===0){
+      if (CartList.children.length === 0) {
         //remove all again reset the elements '
         CartList.remove();
-        //Create again the default 
-        DisplayItemAddcart.innerHTML=`<img src="assets/icons8.png" alt="reset" width='50' height='50' /> 
+        //Create again the default
+        DisplayItemAddcart.innerHTML = `<img src="assets/icons8.png" alt="reset" width='50' height='50' /> 
         <span> No Items Added</span>
-        <span> Add Items From The Service bar Showing Left  </span>`
-     pc=0;
-      PriceShowFtbar.innerText = `$0.00`;
+        <span> Add Items From The Service bar Showing Left  </span>`;
+        pc = 0;
+        PriceShowFtbar.innerText = `$0.00`;
       }
-  
     }
   });
+});
 
+let OdrBtn = document.getElementById("Order_Button");
+let UsrNidput = document.getElementById("UsrNidF2");
 
+OdrBtn.addEventListener("click", () => {
+  if (UsrNidput.value.trim() === "") {
+    alert("Please fill out all field ");
+    return;
+  }
+  if (!DisplayItemAddcart.contains(CartList)) {
+    alert("Please add items to the cart");
+    return;
+  }
+  //Proceed Transaction
+  
+  if (!DisplayItemAddcart.contains(popup)) {
+    popup.innerText = "Email has been sent succesfully";
+    UserAcCollection.appendChild(popup);
+    requestAnimationFrame(() => {
+      popup.style.transition = "opacity 0.6s ease";
+      UserAcCollection.style.paddingBottom = "0px";
+      popup.style.opacity = "1";
+    });
+  }
 });
